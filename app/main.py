105235b759ab28806b64f1af6beec1f5ed90eb50
SMIT-MAKER <harpalsmi4333@gmail.com>
2024-02-17 11:45:39 +0530

    same changes alembic api

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Union
+import os
 import subprocess
 
 from fastapi import FastAPI, HTTPException
@@ -43,11 +43,22 @@ app = create_app()
 def read_root():
     return {"Hello": "World"}
 
+
+
+def find_alembic_ini(start_dir):
+    # Walk through the directory tree to find alembic.ini file
+    for root, dirs, files in os.walk(start_dir):
+        if 'alembic.ini' in files:
+            return os.path.join(root, 'alembic.ini')
+    raise FileNotFoundError("alembic.ini not found")
+
 @app.post("/upgrade_alembic")
 async def upgrade_alembic():
     try:
+        # Find alembic.ini file in the current directory
+        alembic_ini_path = find_alembic_ini(os.getcwd())
         # Run the command using subprocess
-        result = subprocess.run(["alembic", "upgrade", "head"], capture_output=True, text=True)
+        result = subprocess.run(["alembic", "--config", alembic_ini_path, "upgrade", "head"], capture_output=True, text=True)
         # Check if the command was successful
         if result.returncode == 0:
             return {"message": "Alembic upgrade successful"}
